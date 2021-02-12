db23e310c723f40208a755bc0a8dcf3a6c07f14f
Herobone <Theherobone@gmail.com>
2021-02-12 17:51:20 +0100

    bot as container

--- discordBot.tf
+++ discordBot.tf
@@ -1,27 +1,59 @@
+# Create service account to run service with no permissions
+resource "google_service_account" "discord" {
+  account_id   = "discord"
+  display_name = "discord"
+}
+
+module "gce-container-discord-bot" {
+  source = "terraform-google-modules/container-vm/google"
+  version = "~> 2.0"
+
+  container = {
+    image="gcr.io/google-samples/hello-app:1.0"
+    env = [
+      {
+        VM_NAME = google_compute_instance.minecraft.name
+        DISCORD_TOKEN = var.discord_token
+      }
+    ]
+  }
+
+  restart_policy = "Always"
+}
+
+resource "google_compute_network" "discord" {
+  name = "discord"
+}
+
 resource "google_compute_instance" "discord-bot" {
   depends_on = [ local_file.startup_script ]
   name         = "discord-bot"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
   tags         = ["discord"]
+  allow_stopping_for_update = true
 
   metadata_startup_script = "/var/bot/start.sh;"
 
   metadata = {
-    enable-oslogin = "TRUE"
+    gce-container-declaration = module.gce-container-discord-bot.metadata_value
   }
       
   boot_disk {
-    auto_delete = true # Keep disk after shutdown (game data)
+    auto_delete = true
     initialize_params {
-      image = "debian-cloud/debian-10"
+      image = module.gce-container-discord-bot.source_image
     }
   }
 
   network_interface {
-    network = google_compute_network.minecraft.name
+    network = google_compute_network.discord.name
     access_config {
     }
+  }
+
+  labels = {
+    container-vm = module.gce-container-discord-bot.vm_container_label
   }
 
   scheduling {
@@ -29,8 +61,8 @@ resource "google_compute_instance" "discord-bot" {
     automatic_restart = true
   }
 
-  provisioner "file" {
-    source = "tmp/bot"
-    destination = "/var"
+  service_account {
+    email  = google_service_account.discord.email
+    scopes = ["default", "compute-rw"]
   }
 }
