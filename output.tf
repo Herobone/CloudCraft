f6b3d151705d47e04254f01772cc46f5a1dbb990
Herobone <theherobone@gmail.com>
2021-02-13 11:09:38 +0100

    Show container image Url

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "minecraft_ip" {
   value = data.google_compute_instance.minecraft.network_interface.0.access_config.0.nat_ip
 }
+output "container_image_url" {
+  value = data.google_container_registry_image.discord-gcp-bot.image_url
+}
