499d312f704bb5d869eea659f5c0ff7012112004
Herobone <theherobone@gmail.com>
2021-02-21 23:19:06 +0100

    Remove DNS

--- output.tf
+++ output.tf
@@ -1,6 +1,3 @@
-output "minecraft_ip" {
-  value = data.google_compute_instance.minecraft.network_interface.0.access_config.0.nat_ip
-}
 output "container_image_url" {
   value = data.google_container_registry_image.discord-gcp-bot.image_url
 }
